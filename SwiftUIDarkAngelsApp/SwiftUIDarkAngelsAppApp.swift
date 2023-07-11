83fbbe1af027dcf908f15ebbff298d9197c34e05
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-07-11 14:04:55 +0300

    Initial Commit

---
+++ SwiftUIDarkAngelsApp/SwiftUIDarkAngelsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDarkAngelsAppApp.swift
+//  SwiftUIDarkAngelsApp
+//
+//  Created by Anton Vassel on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDarkAngelsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
